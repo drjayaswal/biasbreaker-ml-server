2c8be0bcb8ffc7c97e662ba8168d2f6dda53f4f8
Dhruv Ratan Jayaswal <mrdhruv.professional@gmail.com>
2026-01-25 19:57:31 +0530

    patch x1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-import io
-from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from sample import data
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+class FileData(BaseModel):
+    filename: str
+    words: List[str]
+    description: str
 
 app = FastAPI()
 
@@ -9,6 +13,22 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to 2B Model"}
 
-@app.get("/get-file")
-def analyseFile():
-    return data[0]
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+@app.post("/analyze")
+async def analyze(data: FileData):
+    input_text = " ".join(data.words)
+    
+    vectorizer = TfidfVectorizer(stop_words='english')
+    
+    tfidf_matrix = vectorizer.fit_transform([input_text,data.description])
+    
+    similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    
+    return {
+        "filename": data.filename,
+        "match_score": round(float(similarity), 4),
+        "status": "High Match" if similarity > 0.8 else "Low Match"
+    }
